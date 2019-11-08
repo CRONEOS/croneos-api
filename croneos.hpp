dc4b778c28b014418ef20e89b7155af554213154
unknown <kasperkasperfish@gmail.com>
2019-11-08 14:21:06 +0100

    moved some code around

--- croneos.hpp
+++ croneos.hpp
@@ -11,9 +11,21 @@
 namespace croneos{
     using namespace std;
     using namespace eosio;
-     
-    struct job{
 
+    //todo fetch stuff from the cron_contract
+    
+    name const cron_contract_name = name("piecestest12");
+    permission_level const required_exec_permission_level = permission_level{"execexecexec"_n, "active"_n};
+
+    void deposit(name owner, asset gas, permission_level auth){
+
+        if(gas.amount > 0){
+            action(auth, "eosio.token"_n, "transfer"_n, make_tuple(owner, cron_contract_name, gas, string("pay gas"))).send();
+        }
+    };
+
+    struct job{
+        //job configs configs
         name owner;//owner and ram payer for storing the cronjob
         name tag = name(0);
         time_point_sec due_date = time_point_sec(0);
@@ -22,11 +34,8 @@ namespace croneos{
         uint32_t expiration_sec = 0;
         asset gas_fee = asset(0, symbol(symbol_code("EOS"), 4));//optional: the gas fee you are willing to pay
         string description ="This is the default description";//optional:describe the cronjob, visible in UI
-
-        vector<permission_level> exec_permission_level = {permission_level{"execexecexec"_n, "active"_n} };//permission level used for executing the cronjob
-
+        vector<permission_level> exec_permission_level = { required_exec_permission_level };
         bool auto_pay_gas = false;
-        
 
         template<typename... T>
         void schedule(name code, name actionname, tuple<T...> data, permission_level auth) {
@@ -37,28 +46,15 @@ namespace croneos{
 
             //pay gas
             if(auto_pay_gas){
-                pay_gas(gas_fee, auth);
+                deposit(owner, gas_fee, auth);
             }
             //schedule
             action(
                 auth,
-                "piecestest12"_n, "schedule"_n,
-                 make_tuple(owner, tag, cron_actions, due_date, delay_sec, expiration, expiration_sec, gas_fee, description)
+                cron_contract_name, "schedule"_n,
+                make_tuple(owner, tag, cron_actions, due_date, delay_sec, expiration, expiration_sec, gas_fee, description)
             ).send();
-
             
-        }
-
-        void pay_gas(asset gas, permission_level auth){
-
-            if(gas.amount > 0){
-                action(
-                    auth,
-                    "eosio.token"_n, "transfer"_n,
-                    make_tuple(owner, "piecestest12"_n, gas, string("pay gas"))
-                ).send();
-            }
-        
         }
 
     };
