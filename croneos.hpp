5c8db847badf942d97162a1a06acc5eb2b61a0b2
unknown <kasperkasperfish@gmail.com>
2020-02-05 21:56:05 +0100

    remove namespaces

--- croneos.hpp
+++ croneos.hpp
@@ -9,39 +9,49 @@
 #include <eosio/permission.hpp>
 
 namespace croneos{
-    using namespace std;
-    using namespace eosio;
+    //using namespace std;
+    //using namespace eosio;
 
-    //todo fetch stuff from the cron_contract
+    //This name space can be savely removed (if not using it's members) to save RAM on the host contract/account
+    namespace utils{
+        eosio::checksum256 get_trx_id(){
+            auto size = eosio::transaction_size();
+            char* buffer = (char*)( 512 < size ? malloc(size) : alloca(size) );
+            uint32_t read = eosio::read_transaction( buffer, size );
+            eosio::check( size == read, "ERR::READ_TRANSACTION_FAILED::read_transaction failed");
+            eosio::checksum256 trx_id = eosio::sha256(buffer, read);
+            return trx_id;
+        }
+    }
 
-    name const cron_contract_name = name("piecestest12");
-    permission_level const required_exec_permission_level = permission_level{"execexecexec"_n, "active"_n};
+    eosio::name const cron_contract_name = eosio::name("piecestest12");
+    eosio::permission_level const required_exec_permission_level = eosio::permission_level{"execexecexec"_n, "active"_n};
 
-    void deposit(name owner, asset gas, permission_level auth){
+    void deposit(eosio::name owner, eosio::asset gas, eosio::permission_level auth){
 
         if(gas.amount > 0){
-            action(auth, "eosio.token"_n, "transfer"_n, make_tuple(owner, cron_contract_name, gas, string("pay gas"))).send();
+            eosio::action(auth, "eosio.token"_n, "transfer"_n, make_tuple(owner, cron_contract_name, gas, std::string("deposit gas"))).send();
         }
     };
 
     struct job{
         //job configs configs
-        name owner;//owner and ram payer for storing the cronjob
-        name tag = name(0);
-        time_point_sec due_date = time_point_sec(0);
+        eosio::name owner;//owner and ram payer for storing the cronjob
+        eosio::name tag = eosio::name(0);
+        eosio::time_point_sec due_date = eosio::time_point_sec(0);
         uint32_t delay_sec = 0;
-        time_point_sec expiration = time_point_sec(0);
+        eosio::time_point_sec expiration = eosio::time_point_sec(0);
         uint32_t expiration_sec = 0;
-        asset gas_fee = asset(0, symbol(symbol_code("EOS"), 4));//optional: the gas fee you are willing to pay
-        string description ="This is the default description";//optional:describe the cronjob, visible in UI
-        vector<permission_level> custom_exec_permissions;
+        eosio::asset gas_fee = eosio::asset(0, eosio::symbol(eosio::symbol_code("EOS"), 4));//optional: the gas fee you are willing to pay
+        std::string description ="This is the default description";//optional:describe the cronjob, visible in UI
+        std::vector<eosio::permission_level> custom_exec_permissions;
         bool auto_pay_gas = false;
 
         template<typename... T>
-        void schedule(name code, name actionname, tuple<T...> data, permission_level auth) {
+        void schedule(eosio::name code, eosio::name actionname, std::tuple<T...> data, eosio::permission_level auth) {
             
-            vector<action> cron_actions;
-            action cron_action = action(construct_permission_levels(), code, actionname, move(data) );
+            std::vector<eosio::action> cron_actions;
+            eosio::action cron_action = eosio::action(construct_permission_levels(), code, actionname, move(data) );
             cron_actions.push_back(cron_action);
 
             //pay gas
@@ -49,7 +59,7 @@ namespace croneos{
                 deposit(owner, gas_fee, auth);
             }
             //schedule
-            action(
+            eosio::action(
                 auth,
                 cron_contract_name, "schedule"_n,
                 make_tuple(owner, tag, cron_actions, due_date, delay_sec, expiration, expiration_sec, gas_fee, description)
@@ -57,7 +67,7 @@ namespace croneos{
             
         }
         private:
-        vector<permission_level> construct_permission_levels(){
+        std::vector<eosio::permission_level> construct_permission_levels(){
             custom_exec_permissions.push_back(required_exec_permission_level);
             return custom_exec_permissions;
         }
